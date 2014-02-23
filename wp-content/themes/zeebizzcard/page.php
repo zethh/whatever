dec55d3ec429d06b5f9fb2cc7e1dc16d933c1a7f
voicis <davis.voicescuks@gmail.com>
2014-02-23 15:29:05 -0800

    Not displaying forum heading anymore.

--- wp-content/themes/zeebizzcard/page.php
+++ wp-content/themes/zeebizzcard/page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
 	<div id="content">
-		
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
 			<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
-				<h2 class="page-title"><?php the_title(); ?></h2>
+				<?php $title = get_the_title();
+				if ($title != "Forum"){
+					echo '<h2 class="page-title">' . $title . '</h2>';
+				}
+				?>
 				<?php edit_post_link(__( 'Edit', 'themezee_lang' )); ?>
 
 				<div class="entry">
